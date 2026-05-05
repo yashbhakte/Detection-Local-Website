48fc737e6638545c390f40fbea2853b33a8cbe27
yashbhakte <yashbhakte4@gmail.com>
2026-05-05 17:15:11 +0530

    Updated

--- backend/app.py
+++ backend/app.py
@@ -35,8 +35,14 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False, # Changed to False for better compatibility with wildcard origins
+    allow_origins=[
+        "https://fabric-dd-bbfrontend.onrender.com",
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:5501",
+        "http://localhost:5500",
+        "http://localhost:5501",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
